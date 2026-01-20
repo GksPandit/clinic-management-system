61e21dbe750c1e8ea2386c18c82b79b02541f691
GksPandit <jikeshkumar841439@gmail.com>
2026-01-20 22:18:10 +0530

    Fix App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import DoctorDashboard from "./pages/DoctorDashboard";
 import ReceptionistDashboard from "./pages/ReceptionistDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+        <Route
+          path="/doctor"
+          element={
+            <ProtectedRoute role="doctor">
+              <DoctorDashboard />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/doctor"
-            element={
-              <ProtectedRoute allowedRole="doctor">
-                <DoctorDashboard />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/receptionist"
-            element={
-              <ProtectedRoute allowedRole="receptionist">
-                <ReceptionistDashboard />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </AuthProvider>
+        <Route
+          path="/receptionist"
+          element={
+            <ProtectedRoute role="receptionist">
+              <ReceptionistDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
